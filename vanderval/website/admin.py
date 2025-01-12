09f33dd6dafbe235c4e0841bb25e10767742ac10
AshutoshAgnihotri <ashutoshagnihotri@cdac.in>
2025-01-12 14:08:39 +0530

    updated admin

--- vanderval/website/admin.py
+++ vanderval/website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Site, UserRecords
+from .models import Site, UserRecords,Job
 
 admin.site.register(Site)
 admin.site.register(UserRecords)
+admin.site.register(Job)
